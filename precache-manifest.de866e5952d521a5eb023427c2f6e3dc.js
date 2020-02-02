b8fadc80d3fafd8bff0deda9354e867938ba0ecb
Natalia <47737431+elline777@users.noreply.github.com>
2020-02-02 17:51:09 +0500

    Updates

---
+++ precache-manifest.de866e5952d521a5eb023427c2f6e3dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2c69cfdd474dc73f2ef8d20e1ec9c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af6003622edbc90b6854",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "64ef9f9ea8a08ebcd8eb",
+    "url": "/static/js/2.c588ade7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c588ade7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af6003622edbc90b6854",
+    "url": "/static/js/main.662d1e5f.chunk.js"
+  },
+  {
+    "revision": "249a37921e0703ab7925",
+    "url": "/static/js/runtime-main.04abcc89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
